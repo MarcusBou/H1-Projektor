39df152c1123e8345ec49bec49529a9b44dfc21c
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-01-29 12:11:33 +0100

    Ello

--- ARRAY/Program.cs
+++ ARRAY/Program.cs
@@ -10,7 +10,17 @@ namespace ARRAY
     {
         static void Main(string[] args)
         {
-            
+            int[] arr = new int[10];
+
+            //et for loop til at fylde arrayet
+            for (int i = 1; i <= arr.Length - 1; i++)
+            {
+                //tilføjer værdier til arrayet
+                arr[i] = i;
+                arr[5] = arr[4] * 2;
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
