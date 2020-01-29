9b04b8dd7716f70bdefd63b1483845280d2fbb68
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-01-29 13:24:46 +0100

    Updata

---
+++ Array øvelse 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_øvelse_1
+{
+    class Program
+    {
+        public static string[] Bruger = { "Root", "Bruger", "admin" };
+        public static string[] Pass = { "root", "bruger24", "admindaway" };
+        static void Main(string[] args)
+        {
+            string tryBruger, tryPass;
+            int passforsøg = 3;
+            int id = 0;
+            bool brugerRigtig = false;
+            bool passwordRigtig = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Login");
+                //tager imod og ser om din bruger findes
+                Console.Write("\nBruger: ");
+                tryBruger = Console.ReadLine();
+                //tjekker om brugeren findes i arrayet
+                for (int i = 0; i < Bruger.Length; i++)
+                {
+                    if (tryBruger == Bruger[i])
+                    {
+                        brugerRigtig = true;
+                        id = i;
+                    }
+                }
+
+            } while (!brugerRigtig);
+
+            do
+            {
+                //ser hvilket password du skriver
+                Console.Write("Passsword: ");
+                tryPass = Console.ReadLine();
+                //tjekker om dit password passer i arrayet
+                for (int k = 0; k <= passforsøg - 1; k++)
+                {
+                    if (tryPass == Pass[id])
+                    {
+                        Console.WriteLine("Hvad gør du så nu?");
+                        passwordRigtig = true;
+                        k = 3;
+                    }
+                }
+            } while (!passwordRigtig);
+
+            Console.ReadKey();
+        }
+    }
+}
