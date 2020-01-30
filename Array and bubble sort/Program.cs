ad98849b2ccee9b907f0df0edd7983a6b3c24c59
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-01-30 15:44:17 +0100

    updatee

---
+++ Array and bubble sort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_and_bubble_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrTal = new int[100];
+            Random rnd = new Random();
+
+            for (int i = 0; i < arrTal.Length; i++)
+            {
+                int randomNumb = rnd.Next(0,500);
+                arrTal[i] = randomNumb;
+                for (int j = 0; j < arrTal.Length - 1; j++)
+                {
+                    if (arrTal[j] > arrTal[j+1])
+                    {
+                        int temp = arrTal[j + 1];
+                        arrTal[j + 1] = arrTal[j];
+                        arrTal[j] = temp;
+                    }
+                }
+      
+                Console.WriteLine(arrTal[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
