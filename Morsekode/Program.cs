662bc1767e100da9a21435d894300b0460796922
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-01-30 10:03:13 +0100

    udate again

---
+++ Morsekode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morsekode
+{
+    class Program
+    {
+        static char[] alfabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p' , 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å' };
+        static string[] morsekode = { ".-", "-...", "-.-.", "-..", ".", "..-", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".-.-", "---", ".--.-" };
+        static void Main(string[] args)
+        {
+            string sætning;
+
+            Console.Write("hvad ønsker du oversat til morsekode: ");
+            sætning = Console.ReadLine();
+            char[] sætningArr = sætning.ToCharArray();
+
+            for (int i = 0; i < sætningArr.Length; i++)
+            {
+                Console.WriteLine(sætningArr[i]);
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ array øvelse 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_øvelse_2
+{
+    class Program
+    {
+
+        public static string[] drengenavne = { "William", "Oliver", "Noah", "Emil", "Victor", "Magnus", "Frederik", "Mikkel", "Lucas", "Alexander", "Oscar", "Mathias", "Sebastian", "Malthe", "Elias", "Christian", "Mads", "Gustav", "Villads", "Tobias", "Anton", "Carl", "Silas", "Valdemar", "Benjamin", "Nikolaj", "Marcus", "August", "Sander", "Jacob"  };
+
+        static void Main(string[] args)
+        {
+            string søg;
+            Console.Write("søg: ");
+            søg = Console.ReadLine();
+            Console.WriteLine("\n");
+   
+
+            for (int i = 0; i < drengenavne.Length; i++)
+            {
+
+                if (drengenavne.Contains(søg))
+                {
+                    Console.WriteLine("den findes i data basen");
+                }
+                drengenavne.Contains(søg);
+                Array.Sort(drengenavne);
+                Console.WriteLine(drengenavne[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
