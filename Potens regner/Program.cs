378840c80f32e206c1c84b86241f34d6033c7f70
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-01-31 10:31:54 +0100

    Update

---
+++ Potens regner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potens_regner
+{
+    class Program
+    {
+         public static double tal, potens, output;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Dit primære tal: ");
+            Double.TryParse(Console.ReadLine(), out tal);
+            Console.Write("Din potens: ");
+            Double.TryParse(Console.ReadLine(), out potens);
+
+            potenser();
+            Console.ReadKey();
+        }
+
+        static void potenser()
+        {
+            output = tal;
+            for (int i = 0; i < potens-1; i++)
+            {
+                output = output * tal;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
