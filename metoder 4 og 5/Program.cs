10fe2fa774b20f9b7c795990f8518473d9904c42
Marcus Damgaard Bournonwille <marc8847@zbc.dk>
2020-01-31 11:59:28 +0100

    update

---
+++ metoder 4 og 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metoder_4_og_5
+{
+    class Program
+    {
+        public static int[] reverse = new int[9];
+        public static int[] normal = new int[9];
+        static void Main(string[] args)
+        {
+            valueLoop();
+            printValue();
+            Console.ReadKey();
+        }
+
+        public static void valueLoop()
+        {
+            for (int i = 0; i <= normal.Length -1; i++)
+            {
+                normal[i] = i + 1;
+                reverse[i] = i + 1;
+            }
+        }
+
+        public static void printValue()
+        {
+            for (int i = 0; i <= normal.Length -1; i++)
+            {
+                Console.WriteLine(normal[i]);
+            }
+            Console.WriteLine("\n");
+            Array.Reverse(reverse);
+            for (int i = 0; i <= reverse.Length - 1; i++)
+            {
+                Console.WriteLine(reverse[i]);
+            }
+        }
+    }
+}
